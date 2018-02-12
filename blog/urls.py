1323dccc4b7a7df95b69a9c563dda61e8610bd5b
Svinopasov <sunn2422@gmail.com>
2018-02-12 21:31:38 +0300

    django completed

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,7 @@ from blog.models import Post
 
 urlpatterns = [
     path('', ListView.as_view(queryset=Post.objects.all().order_by("-date")[:25],
-    template_name="blog/blog.html"))]
+    template_name="blog/blog.html")),
+    path('<int:pk>/', DetailView.as_view(model=Post,
+    template_name='blog/post.html'))
+    ]
