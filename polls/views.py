8564b26f0244699ccf9d4259af165d285b242f9e
HegemonX <sunn2422@gmail.com>
2018-02-18 21:22:06 +0300

    "hard" polls end

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Question
+from .models import Question, Choice
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect
 # Create your views here.
 
 def index(request):
@@ -21,7 +23,26 @@ def detail(request, question_id):
     return render(request, template, context)
 
 def results(request, question_id):
-    return HttpResponse("Results %s" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {
+        'question': question,
+    })
 
 def vote(request, question_id):
-    return HttpResponse("Voting %s" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(
+        pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select the choice(.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+    #Returning HttpResponceRedirrect after successfully
+    #dealing with POST data. This prevents data from being
+    # posted twice if a user hits the 'Back' button.HttpResponseRedirect
+        return HttpResponseRedirect(reverse('polls:results',
+        args=(question.id,)))
