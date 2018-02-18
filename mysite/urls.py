18fe860a83eb736a2cb75459029902b9b30dbcbd
HegemonX <sunn2422@gmail.com>
2018-02-18 17:06:23 +0300

    commit_late

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('personal.urls')),
     path('blog/', include('blog.urls')),
+    path('polls/', include('polls.urls')),
 ]
---
+++ polls/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Question
+# Create your views here.
+
+def index(request):
+    latest_quetions_list = Question.objects.order_by('-date')[:10]
+    template = 'polls/index.html'
+    context = {'latest_question_list': latest_quetions_list,
+    }
+    return render(request, template, context)
+
+def detail(request, question_id):
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("404\nQuestion not found")
+    question = get_object_or_404(Question, pk=question_id)    
+    template = 'polls/detail.html'
+    context = {'question': question}
+    return render(request, template, context)
+
+def results(request, question_id):
+    return HttpResponse("Results %s" % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Voting %s" % question_id)
