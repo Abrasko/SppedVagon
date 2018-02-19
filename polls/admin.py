2a7b0b4e48a04f3cd13a92d9c412f4951ebbbc14
HegemonX <sunn2422@gmail.com>
2018-02-19 22:21:53 +0300

    base tutor complete

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,21 @@ from django.contrib import admin
 from .models import Question, Choice
 # Register your models here.
 
-admin.site.register(Question)
-admin.site.register(Choice)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 1
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,                  {'fields':  ['question_text'] }),
+        ('Date information',    {'fields':  ['date'],
+                                #  'classes': ['collapse'] 
+                            }),
+            ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'date', 'was_published_recently')
+    list_filter = ['date']
+    search_fields = ['question_text']
+    
+
+admin.site.register(Question, QuestionAdmin)
