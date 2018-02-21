926ed79d83391ede80b12ca8d6c5451cbf7af5a7
HegemonX <sunn2422@gmail.com>
2018-02-21 21:54:43 +0300

    sasas

--- login/tests.py
+++ login/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+import datetime
+from django.utils import timezone
+from django.test import TestCase
+from django.urls import reverse
 
+from django.contrib.auth.models import User
 # Create your tests here.
+
+
+# def create_user(user_name, user_email, password):
+#     try:
+#         user = User(username=user_name)
+#         user = User()
+
